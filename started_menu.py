a7d1571584802cbd6e7e7237747b474d9e47f782
H04ka <67304178+H04ka@users.noreply.github.com>
2024-12-10 19:39:01 +0500

    Add files via upload

---
+++ started_menu.py
@@ -0,0 +1,19 @@
+from reg import *
+from auth import *
+import json
+from keyboard_folder import keyboard
+from dop_info import*
+
+def start_menu():
+    while True:
+        start_input = input("Зарегистрироваться(R)/Войти в аккаунт(A)")
+        if start_input is 'R':
+            registration()
+            break
+        elif start_input is "A":
+            authorization()
+            break
+        else:
+            print("Некорректный ввод, повторите снова")
+start_menu()
+vopros()
